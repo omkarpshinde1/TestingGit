b7b0e86130723abf8f85e125bb680581a00a1dc8
omkarpshinde1 <omkarpshinde1@gmail.com>
2021-05-26 08:54:56 +0530

    My First Commit

---
+++ MyFirstCsharp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyFirstCsharp
+{
+    class Omkar
+    {
+
+        public static void m1()
+        {
+            Console.WriteLine("I am in m1");
+            m2();
+            
+            
+        }
+
+        public static void m2()
+        {
+            Console.WriteLine("I am in m2");
+
+        }
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This Is My First C# Programme.....!!!!");
+           Omkar obj = new Omkar();
+            m1();
+           
+
+
+        }
+    }
+}
+
+
+
